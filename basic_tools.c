5b122776064c27194f5a5b7a09837b0a6b18479e
jacobscuzzi <git@github.com>
2024-10-29 03:37:39 +0100

    adding to parcing :

--- basic_tools.c
+++ basic_tools.c
@@ -6,14 +6,58 @@
 /*   By: jbaumfal <jbaumfal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 00:52:22 by jbaumfal          #+#    #+#             */
-/*   Updated: 2024/10/29 00:58:23 by jbaumfal         ###   ########.fr       */
+/*   Updated: 2024/10/29 03:24:29 by jbaumfal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "./include/philo.h"
 
 bool	nbr_check(char *string)
 {
 	int	i;
 
 	i = 0;
+	while (string[i] == ' ')
+		i++;
+	if (!string[i])
+		return (false);
 	while (string[i])
+	{
+		if (string[i] < '0' || string[i] > 9)
+			return (false);
+	}
+	return (true);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	counter;
+	int	negator;
+
+	negator = 1;
+	i = 0;
+	counter = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			negator *= -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]) == 1)
+	{
+		counter = counter * 10 + str[i] - 48;
+		i++;
+	}
+	return (counter * negator);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c < '0' || c > '9')
+		return (0);
+	else
+		return (1);
 }
--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: jbaumfal <jbaumfal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:17:36 by jbaumfal          #+#    #+#             */
-/*   Updated: 2024/10/29 00:36:23 by jbaumfal         ###   ########.fr       */
+/*   Updated: 2024/10/29 03:37:10 by jbaumfal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,26 @@
 # include <sys/wait.h>
 
 
-typedef struct s_table
+typedef struct s_dataset
 {
-	bool	limit;
-	int		seats;
-	int		forks;
-	int		t_die;
-	int		t_eat;
-	int		t_spleep;
-	int		limit;
-}	t_table;
+	bool			limit;
+	unsigned int	seats;
+	unsigned int	forks;
+	unsigned int	t_die;
+	unsigned int	t_eat;
+	unsigned int	t_spleep;
+	unsigned int	nbr_eat;
+}	t_dataset;
 
 typedef struct s_philo
 {
 
-}
+} t_deep
+
+bool	nbr_check(char *string);
+
+int		ft_isdigit(int c);
+int	ft_atoi(const char *str);
+
+
 #endif
